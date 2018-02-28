d7e71c0b0d279ec6aec3f62492e36b70d94cdfb3
Hyunwoo Ko <hyunwooko@google.com>
2018-02-28 17:50:46 +0900

    Added --result-from-gcs flag to "retry" HC command
    Downloads the result zip file form the given URL and retry on that
    result.
    
    Test: > retry --count <> --result-from-gcs=<gs://URL/to/results_.zip>
    Bug: 73305969
    Change-Id: I92bdc7b7d94352051f1ce2a84737f82c742a838b

--- harnesses/host_controller/command_processor/command_retry.py
+++ harnesses/host_controller/command_processor/command_retry.py
@@ -15,14 +15,90 @@
 #
 
 import os
+import zipfile
 
+from host_controller.build import build_provider_gcs
 from host_controller.command_processor import base_command_processor
+
+from vts.utils.python.common import cmd_utils
+
+# Test result file contains invoked test plan results.
+_TEST_RESULT_XML = "test_result.xml"
 
 
 class CommandRetry(base_command_processor.BaseCommandProcessor):
 
     command = "retry"
     command_detail = "Retry last run test plan for certain times."
+
+    def IsResultZipFile(self, zip_ref):
+        """Determines whether the given zip_ref is the right result archive.
+
+        Need to check the number of contents of the zip file since
+        the "log-result_<>.zip" file only contains "test_result.xml",
+        but cannot parsed by vts-tf properly when trying to do the retry.
+
+        Args:
+            zip_ref: ZipFile, reference to the downloaded results_<>.zip file
+
+        Returns:
+            True if the downloaded zip file is usable from vts-fs,
+            False otherwise.
+        """
+        if len(zip_ref.namelist()) > 1:
+            for name in zip_ref.namelist():
+                if _TEST_RESULT_XML in name:
+                    return True
+        return False
+
+    def GetResultFromGCS(self, gcs_result_path, local_results_dir):
+        """Downloads a vts-tf result zip archive from GCS.
+
+        And unzip the file to "android-vts/results/" path so
+        the vts-tf will parse the result correctly.
+
+        Args:
+            gcs_result_path: string, path to GCS file.
+            local_results_dir: string, abs path to the result directory of
+                               currently running vts-tf.
+        Returns:
+            A string which is the name of unzipped result directory.
+            None if the download has failed or the downloaded zip file
+            is not a correct result archive.
+        """
+        gsutil_path = build_provider_gcs.BuildProviderGCS.GetGsutilPath()
+        if not gsutil_path:
+            print("Please check gsutil is installed and on your PATH")
+            return None
+
+        if (not gcs_result_path.startswith("gs://")
+                or not build_provider_gcs.BuildProviderGCS.IsGcsFile(
+                    gsutil_path, gcs_result_path)):
+            print("%s is not correct GCS url." % gcs_result_path)
+            return None
+        if not gcs_result_path.endswith(".zip"):
+            print("%s is not a correct result archive file." % gcs_result_path)
+            return None
+
+        if not os.path.exists(local_results_dir):
+            os.mkdir(local_results_dir)
+        copy_command = "%s cp %s %s" % (gsutil_path, gcs_result_path,
+                                        local_results_dir)
+        stdout, stderr, err_code = cmd_utils.ExecuteOneShellCommand(
+            copy_command)
+        if err_code != 0:
+            print("Error in copy file from %s (code %s)." % err_code)
+            return None
+        result_zip = os.path.join(local_results_dir,
+                                  gcs_result_path.split("/")[-1])
+        with zipfile.ZipFile(result_zip, mode="r") as zip_ref:
+            if self.IsResultZipFile(zip_ref):
+                unzipped_result_dir = zip_ref.namelist()[0].rstrip("/")
+                zip_ref.extractall(local_results_dir)
+                return unzipped_result_dir
+            else:
+                print("Not a correct vts-tf result archive file.")
+                return None
 
     # @Override
     def SetUp(self):
@@ -32,6 +108,12 @@ class CommandRetry(base_command_processor.BaseCommandProcessor):
             type=int,
             default=30,
             help="Retry count. Default retry count is 30.")
+        self.arg_parser.add_argument(
+            "--result-from-gcs",
+            help=
+            "Google Cloud Storage URL from which the result is downloaded. "
+            "Will retry based on the fetched result data"
+        )
 
     # @Override
     def Run(self, arg_line):
@@ -48,11 +130,24 @@ class CommandRetry(base_command_processor.BaseCommandProcessor):
         vts_root_path = os.path.dirname(tools_path)
         results_path = os.path.join(vts_root_path, "results")
 
-        former_result_count = len([
-            dir for dir in os.listdir(results_path)
-            if os.path.isdir(os.path.join(results_path, dir))
-            and not os.path.islink(os.path.join(results_path, dir))
-        ])
+        unzipped_result_dir = ""
+        unzipped_result_session_id = -1
+        if args.result_from_gcs:
+            unzipped_result_dir = self.GetResultFromGCS(
+                args.result_from_gcs, results_path)
+            if not unzipped_result_dir:
+                return False
+
+        former_results = [
+            result for result in os.listdir(results_path)
+            if os.path.isdir(os.path.join(results_path, result))
+            and not os.path.islink(os.path.join(results_path, result))
+        ]
+        if unzipped_result_dir:
+            former_results.sort()
+            unzipped_result_session_id = former_results.index(
+                unzipped_result_dir)
+        former_result_count = len(former_results)
 
         if former_result_count < 1:
             print("No test plan has been run yet, former results count is %d" %
@@ -60,7 +155,12 @@ class CommandRetry(base_command_processor.BaseCommandProcessor):
             return False
 
         for result_index in range(retry_count):
-            session_id = former_result_count - 1 + result_index
+            if unzipped_result_session_id >= 0:
+                session_id = unzipped_result_session_id
+                unzipped_result_session_id = -1
+            else:
+                session_id = former_result_count - 1 + result_index
+
             retry_test_command = "test --keep-result -- %s --retry %d" % (
                 self.console.test_result["suite_plan"], session_id)
             self.console.onecmd(retry_test_command)
